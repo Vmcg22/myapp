457d08a644f24c9adb0f1cb54d188227e328602e
Vmcg22 <54409821+Vmcg22@users.noreply.github.com>
2020-09-28 19:30:59 -0500

    Se creó edito la tabla (users), se agregó una migración para (email)

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_28_235759) do
+ActiveRecord::Schema.define(version: 2020_09_29_002626) do
 
   create_table "users", force: :cascade do |t|
-    t.integer "numero"
+    t.string "nombre"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
   end
 
 end
